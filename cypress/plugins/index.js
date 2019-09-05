f1f80a08737bada1e35cae28ab131014204093a0
Murali Srinivasan <murali.srinivasan@greenskycredit.com>
2019-09-05 00:22:35 -0400

    Works fine with Browserify instead of typescript

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,16 @@
-const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
-
-module.exports = on => {
-  on('file:preprocessor', cypressTypeScriptPreprocessor)
+// const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
+// module.exports = on => {
+//   on('file:preprocessor', cypressTypeScriptPreprocessor)
+// }
+const browserify = require( '@cypress/browserify-preprocessor' )
+module.exports = ( on ) => {
+  const options = {
+    browserifyOptions: {
+      extensions: [ '.js', '.ts' ],
+      plugin: [
+        [ 'tsify' ],
+      ],
+    },
+  }
+  on( 'file:preprocessor', browserify( options ) )
 }
